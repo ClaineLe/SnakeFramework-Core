79a3a9a24fab2412c77d5dbee2826ae90b8cda14
ClaineLe <7941324@qq.com>
2021-11-08 12:22:39 +0800

    ## [0.2.9] - 2021-11-08 ###Added - 增加环境变量配置(EnvironmentSetting.cs) - 构建配置(BuilderSetting.cs) - 资源打包编辑器 - UPM依赖：unity打包工具
    ###Modify
    - Setting结构，优化启动流程
    - 框架拓展的程序集名抽至启动设置中填写(BootDriverSetting.cs)
    
    ###Fixed
    - 打包回调为null，在出现打包异常时报错

---
+++ Runtime/Settings/EnvironmentSetting.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace com.snake.framework
+{
+    namespace runtime
+    {
+        [UnityEngine.CreateAssetMenu(fileName = "EnvironmentSetting")]
+        public class EnvironmentSetting : SnakeBasicSetting<EnvironmentSetting>
+        {
+            [Header("资源根目录路径")]
+            public string mResRootPath;
+
+            [Header("构建配置路径")]
+            public string mBuilderSettingPath;
+        }
+    }
+}
